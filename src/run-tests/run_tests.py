308c26bac9a8ab575c2ce1005d13fe32072658fb
Jason Thomas <jthomas.dev@proton.me>
2023-09-27 19:53:30 -0600

    refactor bcolors to its own module

--- src/run-tests/run_tests.py
+++ src/run-tests/run_tests.py
@@ -62,19 +62,7 @@ import pathlib
 from typing import Any
 
 import direct_stdout as stdout
-
-
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    GREEN = '\033[92m'
-    WARNING = '\033[93m'
-    RED = '\033[91m'
-    END = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-c = bcolors
+from bcolors import bcolors as c
 
 
 def format_test_result(test, max_text_len,success=True):
